5a519153688cb2f9ee9533de8c60e35634060796
Nitish sharma <sharmanitish183@gmail.com>
2023-07-18 18:07:21 +0530

    updated js file

--- js/script.js
+++ js/script.js
@@ -18,3 +18,27 @@ if(close){
 }
 
 // --> END
+
+
+//js for single product detail page to slide through images of product --> START
+
+var mainImg = document.getElementById("mainImg");
+        var smallImg = document.getElementsByClassName("small-img");
+
+        smallImg[0].onclick = function(){
+            mainImg.src = smallImg[0].src;
+        }
+
+        smallImg[1].onclick = function(){
+            mainImg.src = smallImg[1].src;
+        }
+
+        smallImg[2].onclick = function(){
+            mainImg.src = smallImg[2].src;
+        }
+
+        smallImg[3].onclick = function(){
+            mainImg.src = smallImg[3].src;
+        }
+// --> END
+
