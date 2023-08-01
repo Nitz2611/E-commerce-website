17ebacb9e13c88f1020ea1226e03747c7986d64e
Nitish sharma <sharmanitish183@gmail.com>
2023-08-01 19:30:32 +0530

    document formatted

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ const close = document.getElementById('close');
 
 
 // js for hamburger sidebar menu --> START
-if(bar){
-    bar.addEventListener('click',()=>{
+if (bar) {
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
     })
 }
 
-if(close){
-    close.addEventListener('click',()=>{
+if (close) {
+    close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
 }
@@ -23,22 +23,22 @@ if(close){
 //js for single product detail page to slide through images of product --> START
 
 var mainImg = document.getElementById("mainImg");
-        var smallImg = document.getElementsByClassName("small-img");
+var smallImg = document.getElementsByClassName("small-img");
 
-        smallImg[0].onclick = function(){
-            mainImg.src = smallImg[0].src;
-        }
+smallImg[0].onclick = function () {
+    mainImg.src = smallImg[0].src;
+}
 
-        smallImg[1].onclick = function(){
-            mainImg.src = smallImg[1].src;
-        }
+smallImg[1].onclick = function () {
+    mainImg.src = smallImg[1].src;
+}
 
-        smallImg[2].onclick = function(){
-            mainImg.src = smallImg[2].src;
-        }
+smallImg[2].onclick = function () {
+    mainImg.src = smallImg[2].src;
+}
 
-        smallImg[3].onclick = function(){
-            mainImg.src = smallImg[3].src;
-        }
+smallImg[3].onclick = function () {
+    mainImg.src = smallImg[3].src;
+}
 // --> END
 
